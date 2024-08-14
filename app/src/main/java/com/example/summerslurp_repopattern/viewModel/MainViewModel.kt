80b3beb461172302b05e669040607776887f092c
michaelSyntax <michael.karbe@syntax-institut.de>
2024-08-14 18:44:48 +0200

    Add HomeFragment; Add MainViewModel; Update NavGraph

---
+++ app/src/main/java/com/example/summerslurp_repopattern/viewModel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.summerslurp_repopattern.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+}
